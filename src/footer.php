10f0d833590cce72c8efe2d7efc5a627b16d0585
CRJL1001 <constantlescutprive2@gmail.com>
2026-08-11 16:11:31 +0200

    Delete src directory

--- src/footer.php
+++
@@ -1,8 +0,0 @@
-<section id="footer">
-    <h2>Des Questions ? Appelez le 0800 917 813</h2>
-    <ul>
-        <li>Conditions des cartes cadeaux</li>
-        <li>Conditions d'utilisation</li>
-        <li>Déclaration de confidentialité</li>
-    </ul>
-</section>
